00fc4c237ef7bae634d0dd5d02377c81e82aa2eb
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-17 18:55:44 +0100

    Create ohelp.js

---
+++ commands/ohelp.js
@@ -0,0 +1,21 @@
+const Botconfig = require("../botconfig.json");
+const Discord = require("discord.js");
+//const ms = require("ms")
+
+module.exports.run = async (bot, message, args) => {
+   let oRole = message.guild.roles.find("name", "dev")
+   if(!oRole) return message.reply("You are not authorized to view this.\n If you have permission from dev then please tell them to screen you this help.")
+   if(message.member.roles.has(oRole.id)) {
+   var embed = new Discord.RichEmbed()
+   .setColor("0x1518a8")
+   .setAuthor("<- Your icon, Help for devs.", message.author.avatarURL)
+   .setThumbnail(message.author.avatarURL)
+   .setDescription("FOR DEVS ONLY [HERE](http://www.scp-wiki.net)")
+   message.channel.send(embed)
+   }
+}
+
+module.exports.help = {
+    name: "ohelp",
+    aliases: []
+}
