cbe3f2cb92a632db2d3b1853f3bd607d9106caa5
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-17 18:22:39 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,21 @@
+const Botconfig = require("../botconfig.json");
+const Discord = require("discord.js");
+//const ms = require("ms")
 
+module.exports.run = async (bot, message, args) => {
+   var embed = new Discord.Embed()
+   .setAuthor("Help commands!", message.author.avatarURL)
+   .setColor("GREEN")
+   .setTimestanp()
+   .addField("mod help", "Shows help for moderators!")
+   .addField("general help", "Shows commands for everyone.")
+   .addField("owner help", "Shows help to 3 people only!")
+   .setFooter(message.author.name + " requested this help.");
+   
+   message.channel.send(embed)
+}
+
+module.exports.help = {
+    name: "help",
+    aliases: []
+}
